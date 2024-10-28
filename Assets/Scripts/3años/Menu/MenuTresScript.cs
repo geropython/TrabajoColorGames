88355381a7fbc34f7438e05adc21ffefa519b487
geropython <72422102+geropython@users.noreply.github.com>
2024-10-28 10:57:11 -0300

    Fix
    antes de entrega

--- Assets/Scripts/3años/Menu/MenuTresScript.cs
+++ Assets/Scripts/3años/Menu/MenuTresScript.cs
@@ -9,15 +9,17 @@ public class MenuTresScript : MonoBehaviour
     [SerializeField] private CanvasGroup fadePanelCanvasGroup; // CanvasGroup del fadePanel
     [SerializeField] private GameObject mainPanel; // Panel principal que se mostrará después del fade
     [SerializeField] private float fadeDuration = 1f;  // Para controlar la opacidad del menú
-    [SerializeField] private AudioSource _musicAudioSource;  // Audio de la música
+    [SerializeField] private AudioSource musicSource;
+    private bool isMusicOn = true;
 
     void Start()
     {
+        // Nos aseguramos de que los audios no se reproduzcan al inicio
+        musicSource.Stop();
         // Al iniciar, activamos la intro y el menú pero el menú es invisible
         _menuPanel.SetActive(true);
         IniciarJuegoConFade();
         // Nos aseguramos de que los audios no se reproduzcan al inicio
-        _musicAudioSource.Stop();
     }
 
     #region Fade Coroutines
@@ -43,12 +45,21 @@ public class MenuTresScript : MonoBehaviour
         fadePanelCanvasGroup.gameObject.SetActive(false);
     }
     #endregion
-    
-    // public void OpenWebPage() { Application.OpenURL("https://sites.google.com/view/colorsgames/inicio?authuser=0"); }
+    public void ToggleMusic()
+    {
+        isMusicOn = !isMusicOn;
+        if (isMusicOn)
+        {
+            musicSource.Play();
+        }
+        else
+        {
+            musicSource.Pause();
+        }
+    }
     public void Abecedario() { SceneManager.LoadScene("Abecedario"); }
     public void Album() { SceneManager.LoadScene("Album"); }
     public void Nivel1() { SceneManager.LoadScene("Nivel 1"); }
     public void Nivel2() { SceneManager.LoadScene("Nivel 2"); }
-    public void BackMenu(){ SceneManager.LoadScene("3Menu"); }
-    public void Quit(){Application.Quit();}
+    public void BackMenu(){ SceneManager.LoadScene("MainMenu"); }
 }
